9e8f03c1141a32ad0769429ea81488eca5a00543
Javier Guerrero <j.guerrero@ensolvers.com>
2024-10-21 20:47:08 -0500

    project created

---
+++ src/main/java/com/geo/climate_app/ClimateAppApplication.java
@@ -0,0 +1,13 @@
+package com.geo.climate_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geo/climate_app/ClimateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geo.climate_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
